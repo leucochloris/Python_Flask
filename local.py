50d241e63828537e4214c15a0b43d6bdb8aea16f
Alexandr Shesterikov <108606736+leucochloris@users.noreply.github.com>
2022-08-02 18:26:46 +0300

    Append methods POST and PUT ..... append data and change it

--- local.py
+++ local.py
@@ -1,4 +1,17 @@
 import requests
 
-responce = requests.get("http://127.0.0.1:3000/api/cars/1")   # use GET request - .....requests.get(......
+# responce = requests.get("http://127.0.0.1:3000/api/cars/0")   # use GET request - for receive the data (by the index)
+# responce = requests.delete("http://127.0.0.1:3000/api/cars/2")  # use DELETE request - for delete data (by the index)
+
+# responce = requests.post("http://127.0.0.1:3000/api/cars/5", {"name": "Lada", "count_in_stock": '1'})  # use POST request - for append data (by the index)........
+# responce = requests.post("http://127.0.0.1:3000/api/cars/6", {"name": "Mazerati", "count_in_stock": '3'})  # use POST request - for append data (by the index)
+
+responce = requests.put("http://127.0.0.1:3000/api/cars/5", {"name": "Lada", "count_in_stock": 111})  # use PUT request - for change data (by the index)      ********Unlike the POST, we refer to existing indexes, ONLY we are change value
+responce = requests.put("http://127.0.0.1:3000/api/cars/6", {"name": "Mazerati", "count_in_stock": 321})  # use PUT request - for change data (by the index)  ********Unlike the POST, we refer to existing indexes ONLY we are change value
+
+'''
+We do not use a database, so - every time (depends to method) the data will not be saved
+'''
+
+
 print(responce.json())
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api()
 
 cars = {
-    1: {"name": "BMW", "count in stock": 16},
-    2: {"Name": "Skoda", "count in stock": 5},
-    3: {"Name": "Wolksagen", "count in stock": 31}
+    1: {"name": "BMW", "count_in_stock": 16},
+    2: {"name": "Skoda", "count_in_stock": 5},
+    3: {"name": "Wolksagen", "count_in_stock": 31}
 }
+
 
 class Main(Resource):
     def get(self, cars_id):
@@ -16,6 +17,28 @@ class Main(Resource):
             return cars
         else:
             return cars[cars_id]
+
+    def delete(self, cars_id):
+        del cars[cars_id]
+        return cars
+
+    def post(self, cars_id):
+        pars = reqparse.RequestParser()
+
+        pars.add_argument('name', type=str, location='form')
+        pars.add_argument('count_in_stock', type=int, location='form')
+
+        cars[cars_id] = pars.parse_args()
+        return cars
+
+    def put(self, cars_id):
+        pars = reqparse.RequestParser()
+
+        pars.add_argument('name', type=str, location='form')
+        pars.add_argument('count_in_stock', type=int, location='form')
+
+        cars[cars_id] = pars.parse_args()
+        return cars
 
 
 api.add_resource(Main, "/api/cars/<int:cars_id>")
